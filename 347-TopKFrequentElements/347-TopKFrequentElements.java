04ac32aff624a04a567e2553bfb5f8466e0dd14b
Phuong Thao <phamnpthao0809@gmail.com>
2026-02-25 23:29:37 +0700

    File updated at 2/25/2026, 11:29:37 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,33 +1,16 @@
-// Last updated: 1/7/2026, 12:35:51 AM
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        // hash table count 
-        // sort -> trích ra k phần tử cuối
-        HashMap <Integer, Integer> temp = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            if(temp.containsKey(nums[i])){
-                temp.put(nums[i], temp.get(nums[i]) + 1);
-            } else {
-                temp.put(nums[i], 1);
-            }
-        }
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(temp.entrySet());
-        Collections.sort(entryList, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-        // Tạo mảng kết quả để lưu k phần tử đầu tiên
-        int[] result = new int[k];
-
-        // Trích xuất k phần tử có tần suất cao nhất
-        for (int i = 0; i < k; i++) {
-            result[i] = entryList.get(i).getKey();
-        }
-
-        // Trả về mảng kết quả
-        return result;
-    }
-}
+// Last updated: 2/25/2026, 11:29:37 PM
+1class Solution {
+2    public int[] topKFrequent(int[] nums, int k) {
+3        Map<Integer, Integer> map = new HashMap<>();
+4
+5        for(int x : nums) {
+6            map.put(x, map.getOrDefault(x, 0) + 1);
+7        }
+8        return map.entrySet()
+9                .stream()
+10                .sorted((a, b) -> b.getValue() - a.getValue()) // sort giảm dần
+11                .limit(k)
+12                .mapToInt(Map.Entry::getKey)
+13                .toArray();
+14    }
+15}
