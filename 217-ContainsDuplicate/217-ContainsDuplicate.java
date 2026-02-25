c2e41787b46f7f00af0a5a263cde8fdf9ce9dfed
Phuong Thao <phamnpthao0809@gmail.com>
2026-02-25 21:51:59 +0700

    File updated at 2/25/2026, 9:51:59 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,13 +1,15 @@
-// Last updated: 7/28/2025, 6:14:32 PM
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet <Integer> hashset = new HashSet <Integer>();
-        for(Integer n : nums) {
-            if (hashset.contains(n)) {
-                return true;
-            } 
-            hashset.add(n);
-        }
-        return false;
-    }
-}
+// Last updated: 2/25/2026, 9:51:59 PM
+1class Solution {
+2    public boolean containsDuplicate(int[] nums) {
+3        Map<Integer, Integer> map = new HashMap<>();
+4        for (int x : nums) {
+5            map.put(x, map.getOrDefault(x, 0) + 1);
+6        }
+7        for (Integer key : map.keySet()) {
+8            if (map.get(key) > 1) {
+9                return true;
+10            }
+11        }
+12        return false;
+13    }
+14}
