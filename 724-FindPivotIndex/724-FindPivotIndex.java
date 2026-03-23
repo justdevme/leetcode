1327417438044f838c0599b509dbd62fd6ec1815
Phuong Thao <phamnpthao0809@gmail.com>
2026-03-23 12:34:10 +0700

    File updated at 3/23/2026, 12:34:08 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,19 +1,17 @@
-// Last updated: 3/21/2026, 6:56:49 PM
+// Last updated: 3/23/2026, 12:34:08 PM
 1class Solution {
 2    public int pivotIndex(int[] nums) {
 3        int total = 0;
-4        for(int x : nums) {
-5            total += x;
-6        }
-7        int leftSum = 0;
-8        for(int i = 0; i < nums.length; i++) {
-9            int rightSum = total - leftSum - nums[i]; 
-10            if(leftSum == rightSum) {
-11                return i;
-12            }
-13            leftSum += nums[i];
-14        }
-15        return -1;
-16        
-17    }
-18}
+4        for (int x : nums) total += x;
+5        int leftSum = 0;
+6        for (int i = 0; i < nums.length; i++) {
+7            int rightSum = total - nums[i] - leftSum;
+8
+9            if (leftSum == rightSum) {
+10                return i;
+11            }
+12            leftSum += nums[i];
+13        }
+14        return -1;
+15    }
+16}
