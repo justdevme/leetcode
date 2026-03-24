bf5fe155d82b396daca7c1bf59f381f2496eb79a
Phuong Thao <phamnpthao0809@gmail.com>
2026-03-24 23:46:44 +0700

    File updated at 3/24/2026, 11:46:40 PM

--- 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.java
@@ -1,4 +1,4 @@
-// Last updated: 3/24/2026, 11:46:06 PM
+// Last updated: 3/24/2026, 11:46:40 PM
 1class Solution {
 2    public int numSubarraysWithSum(int[] nums, int goal) {
 3        Map <Integer, Integer> count = new HashMap<>();
@@ -8,7 +8,7 @@
 7
 8        for (int i = 0; i < nums.length; i++) {
 9            sum += nums[i];
-10            res += count.getOrDefault(sum - goal, 0);
+10            res += count.getOrDefault(sum - goal, 0); // kiểm tra xem chọn điểm bắt đầu tại đâu khi kết thúc tại i.
 11            count.put(sum, count.getOrDefault(sum, 0) + 1);
 12        }
 13
