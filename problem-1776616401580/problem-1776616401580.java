2723d7afd257a41ca54e08d54d3dd2889b4bb1bd
Phuong Thao <phamnpthao0809@gmail.com>
2026-04-19 23:33:22 +0700

    Create

---
+++ problem-1776616401580/problem-1776616401580.java
@@ -0,0 +1,26 @@
+// Last updated: 4/19/2026, 11:33:21 PM
+1class Solution {
+2    public int findDuplicate(int[] nums) {
+3        int left = 1;
+4        int right = nums.length - 1; // n
+5
+6        while (left < right) {
+7            int mid = left + (right - left) / 2;
+8
+9            int count = 0;
+10            for (int num : nums) {
+11                if (num <= mid) {
+12                    count++;
+13                }
+14            }
+15
+16            if (count > mid) {
+17                right = mid;
+18            } else {
+19                left = mid + 1;
+20            }
+21        }
+22
+23        return left;
+24    }
+25}
