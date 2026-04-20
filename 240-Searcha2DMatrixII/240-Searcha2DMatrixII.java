476c549c68953b5f1c1ee0b8fa6a1bb283d1e015
Phuong Thao <phamnpthao0809@gmail.com>
2026-04-20 11:02:33 +0700

    File updated at 4/20/2026, 11:02:33 AM

--- 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.java
@@ -1,15 +1,16 @@
-// Last updated: 4/20/2026, 10:59:08 AM
-1class Solution {
-2    public boolean searchMatrix(int[][] matrix, int target) {
-3        for(int i = 0; i < matrix.length; i++){
-4            int l = 0, r = matrix[0].length - 1;
-5            while (l <= r) {
-6                 int mid = (l + r) / 2;
-7                if (matrix[i][mid] == target) return true;
-8                else if (matrix[i][mid] < target) l = mid + 1;
-9                else r = mid - 1;
-10            }
+// Last updated: 4/20/2026, 11:02:33 AM
+1// cách 2 
+2class Solution {
+3    public boolean searchMatrix(int[][] matrix, int target) {
+4        int row = 0;
+5        int col = matrix[0].length - 1;
+6
+7        while (row < matrix.length && col >= 0) {
+8            if (matrix[row][col] == target) return true;
+9            else if (matrix[row][col] > target) col--;
+10            else row++;
 11        }
-12        return false;
-13    }
-14}
+12
+13        return false;
+14    }
+15}
