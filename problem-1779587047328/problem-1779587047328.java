991fce5fdf98f9974b94c5607f8e402ec88478eb
Phuong Thao <phamnpthao0809@gmail.com>
2026-05-24 08:44:06 +0700

    Create

---
+++ problem-1779587047328/problem-1779587047328.java
@@ -0,0 +1,38 @@
+// Last updated: 5/24/2026, 8:44:07 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> inorderTraversal(TreeNode root) {
+18        List<Integer> result = new ArrayList<>();
+19        Stack<TreeNode> stack = new Stack<>();
+20
+21        TreeNode current = root;
+22
+23        while (current != null || !stack.isEmpty()) {
+24            while (current != null) {
+25                stack.push(current);
+26                current = current.left;
+27            }
+28
+29            current = stack.pop();
+30            result.add(current.val);
+31
+32            current = current.right;
+33        }
+34
+35        return result;
+36    }
+37}
