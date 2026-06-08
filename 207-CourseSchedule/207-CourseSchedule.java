78afc9eb7492778f1c086f3fb175d10c50f16b00
Phuong Thao <phamnpthao0809@gmail.com>
2026-06-08 21:03:39 +0700

    File updated at 6/8/2026, 9:03:39 PM

--- 207-CourseSchedule/207-CourseSchedule.java
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -1,52 +1,42 @@
-// Last updated: 6/8/2026, 5:38:39 PM
+// Last updated: 6/8/2026, 9:03:39 PM
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
 3        List<List<Integer>> graph = new ArrayList<>();
-4
-5        for (int i = 0; i < numCourses; i++) {
-6            graph.add(new ArrayList<>());
-7        }
-8
-9        for (int[] pre : prerequisites) {
-10            int course = pre[0];
-11            int prerequisite = pre[1];
-12
-13            // Muốn học course thì phải học prerequisite trước
-14            // nên cạnh là prerequisite -> course
-15            graph.get(prerequisite).add(course);
+4        int[] indeg = new int[numCourses];
+5
+6        for (int i = 0; i < numCourses; i++) {
+7            graph.add(new ArrayList<>());
+8        }
+9
+10        for (int[] p : prerequisites) {
+11            int course = p[0];
+12            int prereq = p[1];
+13
+14            graph.get(prereq).add(course);
+15            indeg[course]++;
 16        }
-17
-18        int[] state = new int[numCourses];
-19
-20        for (int i = 0; i < numCourses; i++) {
-21            if (state[i] == 0) {
-22                if (!dfs(i, graph, state)) {
-23                    return false;
-24                }
-25            }
-26        }
-27
-28        return true;
-29    }
-30
-31    private boolean dfs(int node, List<List<Integer>> graph, int[] state) {
-32        if (state[node] == 1) {
-33            return false; // gặp lại node đang duyệt => có cycle
-34        }
-35
-36        if (state[node] == 2) {
-37            return true; // node này đã xử lý xong rồi
-38        }
-39
-40        state[node] = 1;
-41
-42        for (int next : graph.get(node)) {
-43            if (!dfs(next, graph, state)) {
-44                return false;
-45            }
-46        }
-47
-48        state[node] = 2;
-49        return true;
-50    }
-51}
+17        Queue<Integer> queue = new LinkedList<>();
+18        for (int i = 0; i < numCourses; i++) {
+19            if (indeg[i] == 0) {
+20                queue.offer(i);
+21            }
+22        }
+23
+24        int count = 0;
+25
+26        while (!queue.isEmpty()) {
+27            int u = queue.poll();
+28            count++;
+29
+30            for (int v : graph.get(u)) {
+31                indeg[v]--;
+32
+33                if (indeg[v] == 0) {
+34                    queue.offer(v);
+35                }
+36            }
+37        }
+38
+39        return count == numCourses;
+40    }
+41}
